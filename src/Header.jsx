ac79ceb4d40caa82f93f5db427f3cf68e8506788
choyj2012 <choyj2012@gmail.com>
2023-05-10 19:02:28 +0900

    Header 모양 잡기

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,18 +11,52 @@ export default function Header() {
 const HeaderContents = () => {
   return (
     <div className="header-contents">
-      <div>A</div>
-      <div>B</div>
-      <div>C</div>
+      <Logo />
+      <Search />
+      <UserMenu />
     </div>
   )
 }
 
 const Logo = () => {
   return (
-    <div style={{display: "flex"}}>
-      <button>=</button>
+    <div className="logo">
+      <button className="left-menu-btn">=</button>
       <h1>Calendar</h1>
+    </div>
+  )
+}
+
+const Search = () => {
+  let today = new Date().toLocaleDateString('ko-kr');
+  return (
+    <div className="search-bar">
+      <div className="sb-b">
+        <button>Today</button>
+        <button>{"<"}</button>
+        <button>{">"}</button>
+        <div>{today}</div>
+      </div>
+      <div className="sb-b">
+        <button>Search</button>
+        <button>Setting</button>
+        <select>
+          <option value="month">월</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+const UserMenu = () => {
+  return (
+    <div className="user-menu">
+      <div className="menu-icon">
+        <img src="https://placehold.co/40x40"/>
+      </div>
+      <div className="user-icon">
+        <img src="https://placehold.co/40x40"/>
+      </div>
     </div>
   )
 }
