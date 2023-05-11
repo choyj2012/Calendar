73c14714c068e72eb375e82cd417ccd461ad1504
choyj2012 <choyj2012@gmail.com>
2023-05-11 16:01:50 +0900

    달력 리턴 배열 수정

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,4 +1,16 @@
+let cal = [];
+let tmp = [];
+const addDate = (dateObj) => {
+  tmp.push(dateObj);
+  
+  if(tmp.length == 7){
+    cal.push(tmp);
+    tmp = [];
+  }
+}
+
 const getCalendar = (year, month) => {
+  cal = [];  tmp = [];
   let prevM = new Date(year, month-1, 0);
   let prevDay = prevM.getDay(); //저번 달 마지막 요일
   let prevDate = prevM.getDate(); //저번 달 마지막 날짜
@@ -7,22 +19,21 @@ const getCalendar = (year, month) => {
   let endDate = curM.getDate(); //이번 달 마지막 날짜
   let endDay = curM.getDay(); //이번 달 마지막 요일
 
-  let cal = [];
   //저번 달 날짜 출력 개수 = (prevDay + 1) % 7;
-
   for(let i = (prevDay + 1) % 7; i > 0; i--){
-    cal.push(prevDate - i + 1);
+    addDate({date: prevDate - i + 1, month: 'prev'});
   }
 
   for(let i = 1; i<=endDate; i++){
-    cal.push(i);
+    addDate({date: i, month: 'curr'});
   }
 
   //다음 달 날짜 출력 개수 = 6 - endDay
   for(let i = 1; i <= 6-endDay; i++){
-    cal.push(i);
+    addDate({date: i, month: 'next'});
   }
 
+  console.log(cal);
   return cal;
 }
 
