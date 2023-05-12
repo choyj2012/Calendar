23e5cd544ec3836c3d065059af65e2206664ff95
choyj2012 <choyj2012@gmail.com>
2023-05-12 22:17:59 +0900

    date click시 해당 영역 확장 기능 추가

--- src/Container.jsx
+++ src/Container.jsx
@@ -1,78 +1,152 @@
-import "./Container.css"
-import { getCalendar } from "./Calendar"
+import "./Container.css";
+import { getCalendar } from "./Calendar";
+import { useEffect, useState } from "react";
 
-export default function Container({ym, isLeftOpen}) {
+export default function Container({ ym, isLeftOpen }) {
   return (
     <div className="container-box">
-      <LeftContainer isLeftOpen={isLeftOpen}/>
-      <CenterContainer ym={ym}/>
+      <LeftContainer isLeftOpen={isLeftOpen} />
+      <CenterContainer ym={ym} />
       <RightContainer />
     </div>
-  )
+  );
 }
 
-const LeftContainer = ({isLeftOpen}) => {
-  let className = 'container-left';
-  className += isLeftOpen ? ' left-open' : '';
-  return <div className={className}>Left</div>
-}
+const LeftContainer = ({ isLeftOpen }) => {
+  let className = "container-left";
+  className += isLeftOpen ? " left-open" : "";
+  return <div className={className}>Left</div>;
+};
 
-const CenterContainer = ({ym : {year, month}}) => {
+const CenterContainer = ({ ym: { year, month } }) => {
   const cal = getCalendar(year, month);
+  const [selectedCord, setSelectedCord] = useState({
+    isSelected: false,
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    setSelectedCord(cord => {
+      let newCord = {...cord};
+      newCord.isSelected = false;
+      return newCord;
+    })
+  }, [year, month]);
+  
   return (
     <div className="container-center">
-      <Days />
-      <Month cal={cal}/>
+      <Days selectedCord={selectedCord} />
+      <Month
+        cal={cal}
+        key={year + "" + month}
+        selectedCord={selectedCord}
+        setSelectedCord={setSelectedCord}
+      />
     </div>
-  )
-}
+  );
+};
 
-const Days = () => {
-  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const Days = ({ selectedCord }) => {
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   return (
     <div className="days">
       {days.map((day, i) => {
-        let dayColor = 'weekday';
-        if(i === 0) dayColor = 'sun';
-        if(i === 6) dayColor = 'sat';
-        return <div key={day} className={"day " + dayColor}>{day}</div>
+        let dayColor = "weekday";
+        if (i === 0) dayColor = "sun";
+        if (i === 6) dayColor = "sat";
+        if (selectedCord.isSelected && selectedCord.x === i)
+          dayColor += " selected-width";
+        return (
+          <div key={day} className={"day " + dayColor}>
+            {day}
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-const Month = ({cal}) => {
+const Month = ({ cal, selectedCord, setSelectedCord }) => {
   return (
     <div className="month">
       {cal.map((week, i) => {
-          return <Week key={i} week={week}/>
-        })}
+        let selectedWeek = false;
+        if (selectedCord.isSelected && selectedCord.y === i)
+          selectedWeek = true;
+        return (
+          <Week
+            key={i}
+            week={week}
+            weekNum={i}
+            selectedCord={selectedCord}
+            setSelectedCord={setSelectedCord}
+            selectedWeek={selectedWeek}
+          />
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-const Week = ({week}) => {
+const Week = ({
+  week,
+  weekNum,
+  selectedCord,
+  setSelectedCord,
+  selectedWeek,
+}) => {
   return (
-    <div className="week">
-      {week.map((date, i) => <Date key={i} date={date} day={i}/>)}
-    </div>
-  )
-}
+    <div className={"week" + (selectedWeek ? " selected-height" : "")}>
+      {week.map((date, i) => {
+        let selectedDate = false;
+        if (selectedCord.isSelected && selectedCord.x === i)
+          selectedDate = true;
 
-const Date = ({date, day}) => {
-  let dayColor = 'weekday', numColor = 'basic';
-  if(date.month !== 'curr') numColor = 'light';
-  if(day === 0) dayColor = 'sun';
-  if(day === 6) dayColor = 'sat';
+        return (
+          <Date
+            key={i}
+            weekNum={weekNum}
+            date={date}
+            day={i}
+            setSelectedCord={setSelectedCord}
+            selectedDate={selectedDate}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+const Date = ({ weekNum, date, day, setSelectedCord, selectedDate }) => {
+  let dayColor = "weekday",
+    numColor = "basic";
+  if (date.month !== "curr") numColor = "light";
+  if (day === 0) dayColor = "sun";
+  if (day === 6) dayColor = "sat";
+
+  const handleClick = () => {
+    setSelectedCord((cord) => {
+      let newCord = { ...cord };
+      newCord.x = day;
+      newCord.y = weekNum;
+      newCord.isSelected =
+        newCord.x === cord.x && newCord.y === cord.y
+          ? !newCord.isSelected
+          : true;
+      return newCord;
+    });
+  };
 
   return (
-    <div className="date">
-      <div className={dayColor + ' ' + numColor}>
-        {date.date}
-      </div>
+    <div
+      className={"date" + (selectedDate ? " selected-width" : "")}
+      onClick={handleClick}
+    >
+      <div className={dayColor + " " + numColor}>{date.date}</div>
     </div>
-  )
-}
+  );
+};
 const RightContainer = () => {
-  return <div className="container-right">Right</div>
-}
+  return <div className="container-right">Right</div>;
+};
