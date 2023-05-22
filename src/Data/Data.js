b433657d262c8c6d627d1cb957c1a461f3ce4a28
choyj2012 <choyj2012@gmail.com>
2023-05-22 21:26:07 +0900

    api 추가중

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,14 +1,22 @@
-const getData = () => {
-  return Data;
+import convert from 'xml-js';
+
+const API_KEY = "In89aIkmZCdGv3RzrxGCf8mTRmd6Vb2HziAN%2F54W8cPxBnwsNEKlziWSkpO0aWmoENdpESAYfFF3v2%2Bhx5bVkg%3D%3D"
+const url = "http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService/getRestDeInfo";
+
+const getData = async (year, month) => {
+  if(month < 10) month = '0' + month;
+  getXMLfromAPI(year, month);
 }
 
+const getXMLfromAPI = async (year, month) => {
+  const reqURL = `${url}?solYear=${year}&solMonth=${month}&ServiceKey=${API_KEY}`;
+  const response = await fetch(reqURL);
+  const xmlString = await response.text();
+  const result = convert.xml2json(xmlString, {
+    compact: 'false',
+    spaces: 4,
+  });
+  console.log(result);
+};
 
-
-const Data = [
-  {
-    year: '2023',
-    month: '5',
-    date: '15',
-    title: 'Test1'
-  }
-]
+getData(2023, 5);
