ee9e7ca5cb836176c21c3bf00fe750b672d03662
choyj2012 <choyj2012@gmail.com>
2023-06-05 17:32:37 +0900

    주석제거

--- src/App.jsx
+++ src/App.jsx
@@ -12,16 +12,6 @@ function App() {
     </Routes>
     </BrowserRouter>
   )
-
-  // <BrowserRouter>
-	// 			<Header />
-	// 			<Routes>
-	// 				<Route path="/" element={<Main />}></Route>
-	// 				<Route path="/product/*" element={<Product />}></Route>
-	// 				{/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-	// 				<Route path="*" element={<NotFound />}></Route>
-	// 			</Routes>
-	// 		</BrowserRouter>
 }
 
 export default App
