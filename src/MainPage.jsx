27c6420b87409da8dd018f4cb30267250823dc03
choyj2012 <choyj2012@gmail.com>
2023-06-05 17:31:22 +0900

    MainPage로 분리

---
+++ src/MainPage.jsx
@@ -0,0 +1,27 @@
+import Container from "./Container"
+import Footer from "./Footer"
+import Header from "./Header"
+import { createContext, useMemo, useState } from "react"
+
+export const CurrYmContext = createContext(null);
+
+const MainPage = () => {
+  const [ym, setYM] = useState({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth()+1
+  })
+
+  const [isLeftOpen, setIsLeftOpen] = useState(false);
+  const value = useMemo(() => ym, [ym]);
+  return (
+    <div className="wrap">
+      <CurrYmContext.Provider value={value}>
+        <Header setYM={setYM} setIsLeftOpen={setIsLeftOpen} />
+        <Container isLeftOpen={isLeftOpen} setIsLeftOpen={setIsLeftOpen} />
+      </CurrYmContext.Provider>
+      <Footer />
+    </div>
+  );
+}
+
+export default MainPage
