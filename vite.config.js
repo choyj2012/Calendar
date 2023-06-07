61601b447cb0f2c8059d24d720bca0f8c2fdb145
choyj2012 <choyj2012@gmail.com>
2023-06-07 19:04:09 +0900

    base 수정

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,20 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      'stream': 'stream-browserify'
-    }
-  },
-})
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+    resolve: {
+      alias: {
+        stream: "stream-browserify",
+      },
+    },
+  };
+
+  if (command !== "serve") {
+    config.base = "/Calendar/";
+  }
+
+  return config;
+});
