f5f11635d0aa51a8fc843c47d9ad43a1b4680c4f
choyj2012 <choyj2012@gmail.com>
2023-06-07 18:46:25 +0900

    RouterProvider로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import MainPage from "./MainPage"
 import Login from "./Login"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { createBrowserRouter, RouterProvider} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-
-        <Route path="*" element={<h1>Not Found</h1>}></Route>
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
